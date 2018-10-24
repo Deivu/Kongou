ca8f8f89fe97d546e1769a27a4954db1f2c02812
Deivu <davexdme@gmail.com>
2018-10-24 18:31:05 +0800

    Enhance Moosik Player

--- src/modules/moosikplayer.js
+++ src/modules/moosikplayer.js
@@ -5,8 +5,6 @@ class Player {
 		this.guild = this.Kongou.guilds.get(guildID);
 		this.voiceConnection = this.Kongou.voiceConnections.get(guildID);
 		this.textChannel = this.guild.channels.get(this.queue.textChannel);
-		this.errormethod = this.error.bind(this);
-		this.stream;
 	};
 
 	async start() {
@@ -15,29 +13,29 @@ class Player {
 			this.guild.channels.get(this.voiceConnection.channelID).leave();
 			return;
 		};
-
-		this.stream = this.Kongou.ytdl(this.queue.songs[0].url, { quality: 'highestaudio' });
-		this.voiceConnection.play(this.stream, { inlineVolume: true, sampleRate: 96000 });
+		const stream = this.Kongou.ytdl(this.queue.songs[0].url, { quality: 'highest' });
+		this.voiceConnection.play(stream, { inlineVolume: true, sampleRate: 96000 });
 		this.voiceConnection.setVolume(Math.pow(0.60, 1.660964));
-		await this.textChannel.createMessage(`Admiral, The Current Song is \`\`\`diff\n- ${this.queue.songs[0].title}\`\`\``);
-
+		const onConnectionError = (error) => {
+			this.voiceConnection.stopPlaying();
+			this.voiceConnection.end();
+			this.Kongou.cannons.fire(error);
+		};
+		const onStreamError = (error) => {
+			stream.end();
+			this.Kongou.cannons.fire(error);
+		};
 		this.voiceConnection.once('end', () => {
-			this.voiceConnection.removeListener('error', this.error);
-			this.stream.removeListener('error', this.error);
-			this.stream.destroy();
+			stream.removeListener('error', onStreamError);
+			stream.destroy();
+			this.voiceConnection.removeListener('error', onConnectionError);
 			this.queue.songs.shift();
 			this.start()
-			.catch(err => {
-			    this.Kongou.cannons.fire(err);
-		    });
+			.catch(err => this.Kongou.cannons.fire(err));
 		});
-		this.stream.on('error', this.errormethod);
-		this.voiceConnection.on('error', this.errormethod);
-	};
-	
-	async error(error) {
-		this.voiceConnection.stopPlaying();
-		this.Kongou.cannons.fire(error);
+		stream.on('error', onStreamError);
+		this.voiceConnection.on('error', onConnectionError);
+		await this.textChannel.createMessage(`Admiral, The Current Song is \`\`\`diff\n- ${this.queue.songs[0].title}\`\`\``);
 	};
 };
 
