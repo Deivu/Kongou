06d23ee2bb14a53c7ce9b96c8a202b58b703d785
Deivu <davexdme@gmail.com>
2019-06-18 19:17:59 +0800

    Simplify Dispatcher for easier reading

--- src/modules/KongouDispatcher.js
+++ src/modules/KongouDispatcher.js
@@ -7,38 +7,26 @@ class KongouDispatcher {
         this.link = options.link;
         this.queue = [];
         this.current = null;
-        this.lastUpdate = null;
-
-        this.link.player.on('playerUpdate', (data) => {
-            if (!data.position && !this.lastUpdate) {
-                this.text.send(`Now Playing: **${this.current.info.title}**`).catch(() => null);
-            }
-            this.lastUpdate = data.time;
-        })
+        
         this.link.player.on('end', () => {
-            this.lastUpdate = null;
             this.play()
                 .catch((error) => {
                     console.error(error);
                     this.queue.length = 0;
                     this.leave();
                 });
-          })
-        this.link.player.on('exception', console.error);
+        });
         this.link.player.on('stuck', (reason) => {
             console.warn(reason);
-            this.lastUpdate = null;
             this.play()
                 .catch((error) => {
                     console.error(error);
                     this.queue.length = 0;
                     this.leave();
                 });
-          });
-        this.link.player.on('voiceClose', (reason) => {
-            console.warn(reason);
-            this.leave();
         });
+        this.link.player.on('exception', console.error);
+        this.link.player.on('voiceClose', this.leave.bind(this));
         this.link.player.on('nodeDisconnect', this.leave.bind(this));
     }
 
@@ -50,9 +38,11 @@ class KongouDispatcher {
         if (!this.exists || !this.queue.length) return this.leave();
         this.current = this.queue.shift();
         await this.link.player.playTrack(this.current.track);
+        await this.text.send(`Now Playing: **${this.current.info.title}**`).catch(() => null);
     }
 
-    leave() {
+    leave(log) {
+        if (log) console.log(log);
         this.queue.length = 0;
         this.link.disconnect();
         this.client.queue.delete(this.guild.id);
