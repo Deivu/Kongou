0ec928a9499580077aed8abff3c076cd1d128e73
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-07-16 16:14:40 +0530

    Create Pause.js

---
+++ src/commands/Pause.js
@@ -0,0 +1,27 @@
+const KongouCommand = require('../abstract/KongouCommand.js');
+
+class Pause extends KongouCommand {
+    get name() {
+        return 'pause';
+    }
+
+    get usage() {
+        return 'pause';
+    }
+
+    get description() {
+        return 'pause the song';
+    }
+
+    async run(msg) {
+        if (!msg.member.voice.channelID)
+            return await msg.channel.send('Admiral, you are not in a voice channel to perform this');
+        const dispatcher = this.client.queue.get(msg.guild.id);
+        if (!dispatcher)
+            return await msg.channel.send('Nothing is playing in this guild.');
+        if (dispatcher.player.voiceConnection.voiceChannelID !== msg.member.voice.channelID)
+            return await msg.channel.send('Teitoku, you are not in the same voice channel where I am.');
+        await dispatcher.pause();
+    }
+}
+module.exports = Pause;
