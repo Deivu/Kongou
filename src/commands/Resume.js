c83bbb44f479186317644f4c2cad2e24a17b5b93
rajamoulimallareddy <68652418+rajamoulimallareddy@users.noreply.github.com>
2021-08-04 19:36:02 +0530

    Update Resume.js

--- src/commands/Resume.js
+++ src/commands/Resume.js
@@ -14,12 +14,12 @@ class Resume extends KongouCommand {
     }
 
     async run(msg) {
-        if (!msg.member.voice.channelID)
+        if (!msg.member.voice.channelId)
             return await msg.channel.send('Admiral, you are not in a voice channel to perform this');
         const dispatcher = this.client.queue.get(msg.guild.id);
         if (!dispatcher)
             return await msg.channel.send('Nothing is playing in this guild.');
-        if (dispatcher.player.voiceConnection.voiceChannelID !== msg.member.voice.channelID)
+        if (dispatcher.player.connection.channelID !== msg.member.voice.channelId)
             return await msg.channel.send('Teitoku, you are not in the same voice channel where I am.');
         await dispatcher.resume();
     }
