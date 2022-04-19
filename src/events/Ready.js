2f07f615d40bcf4196c41f3f0848438de744286c
TYSONᴰᴱⱽ <74145740+TysonOP@users.noreply.github.com>
2022-04-19 22:40:16 +0530

    Updated Ready.js

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -13,18 +13,18 @@ class Ready extends KongouEvent {
         this.client.logger.debug(`${this.client.user.username}`, `Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)`);
         if (!this.interval) {
             await this.client.user.setActivity('It\'s the English-born returnee, Kongou! Nice to meet you!');
-            const ActivityType = [
-                `PLAYING`,
-                `PLAYING`,
-                `PLAYING`
-            ]
-            const statuses =  [
-                'Admiral! You\'ve got mail! Love Letters aren\'t allowed!',
-                'Always got to make time for tea-time!',
-                'Burning... Love!'
+          
+            const statuses = [
+                {type : 'PLAYING', message: 'Admiral! You\'ve got mail! Love Letters aren\'t allowed!'},
+                {type: 'LISTENING' , message: 'Always got to make time for tea-time!'},
+                {type: 'PLAYING' , message: 'Burning... Love!'},
             ];
-            let i = 0;
-            setInterval(() => this.client.user.setActivity(`${statuses[i++ % statuses.length]}`, { type: ActivityType[i++ % ActivityType.length] }), 300000);
+        let i = 0;
+        this.interval = setInterval(() => {
+          if(i > statuses.length -1 ) i = 0;
+          this.client.user.setActivity(`${statuses[i].message}`, {type : `${statuses[i].type}`});
+          i++;
+        }, 300000);
             /*
             this.interval = setInterval(() => {
                 const current = statuses.shift();
