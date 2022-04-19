2d87569e41c98ccc4c67430920fd1c2cbc1a99b6
TYSONᴰᴱⱽ <74145740+TysonOP@users.noreply.github.com>
2022-04-19 19:47:08 +0530

    Updated Ready.js

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -13,16 +13,25 @@ class Ready extends KongouEvent {
         this.client.logger.debug(`${this.client.user.username}`, `Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)`);
         if (!this.interval) {
             await this.client.user.setActivity('It\'s the English-born returnee, Kongou! Nice to meet you!');
+            const ActivityType = [
+                `PLAYING`,
+                `PLAYING`,
+                `PLAYING`
+            ]
             const statuses =  [
                 'Admiral! You\'ve got mail! Love Letters aren\'t allowed!',
                 'Always got to make time for tea-time!',
                 'Burning... Love!'
             ];
+            let i = 0;
+            setInterval(() => this.client.user.setActivity(`${statuses[i++ % statuses.length]}`, { type: ActivityType[i++ % ActivityType.length] }), 300000);
+            /*
             this.interval = setInterval(() => {
                 const current = statuses.shift();
                 this.client.user.setActivity(current);
                 statuses.push(current);
             }, 300000);
+            */
         }
     }
 }
